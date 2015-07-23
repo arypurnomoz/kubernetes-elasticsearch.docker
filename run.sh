8d329d5ded1c960a888351c214ca184eb5cd4011
arypurnomoz <arypurnomoz@gmail.com>
2015-07-23 18:47:59 +0700

    update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-/discovery --namespace="${NAMESPACE}" --selector="${SELECTOR}" >> /tmp/elasticsearch.yml
+/discovery --namespace="${NAMESPACE}" --selector="${SELECTOR}" >> /usr/share/elasticsearch/conf/elasticsearch.yml
 
 exec /docker-entrypoint.sh elasticsearch
