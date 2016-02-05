7dbc76aa0a2642e5c0cc6babe9d4bc155920f8fb
arypurnomoz <arypurnomoz@gmail.com>
2016-02-06 00:09:21 +0700

    fix

--- run.sh
+++ run.sh
@@ -22,6 +22,5 @@ CONFIG=/usr/share/elasticsearch/config/elasticsearch.yml
 echo "discovery.zen.ping.unicast.hosts: [$PEER_NODES]" >> $CONFIG
 echo "discovery.zen.ping.multicast.enabled: false" >> $CONFIG
 echo "network.publish_host: `hostname -i`" >> $CONFIG
-echo "network.publish_host: `hostname -i`" >> $CONFIG
 
 exec /docker-entrypoint.sh elasticsearch $@
